9cd5a747cb108a13216d35b5612d988c4adc9e09
Mariya Lucas <mariya.podosinova@gmail.com>
2019-05-31 17:41:36 +0100

    null

--- src/Bookings.js
+++ src/Bookings.js
@@ -7,7 +7,7 @@ export default class Bookings extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      fakeBookings: FakeBookings
+      fakeBookings: null
     };
   }
 
