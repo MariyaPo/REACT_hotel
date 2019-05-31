789de02be17730556de6bfffc4ae5c28ec03b4fb
Mariya Lucas <mariya.podosinova@gmail.com>
2019-05-31 17:37:55 +0100

    class3 done

--- src/Bookings.js
+++ src/Bookings.js
@@ -4,8 +4,38 @@ import SearchResults from "./SearchResults.js";
 import FakeBookings from "./data/fakeBookings.json";
 
 export default class Bookings extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fakeBookings: FakeBookings
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://cyf-react.glitch.me").then(resultOfFetching =>
+      resultOfFetching.json().then(dataFetched =>
+        this.setState({
+          fakeBookings: dataFetched
+        })
+      )
+    );
+  }
+
   search = searchVal => {
-    console.info("TO DO!", searchVal);
+    let filteredSearch = this.state.fakeBookings.filter(
+      filtered =>
+        filtered.firstName.toLowerCase() === searchVal.toLowerCase() ||
+        filtered.surname.toLowerCase() === searchVal.toLowerCase()
+    );
+    if (filteredSearch.length === 0) {
+      this.setState({
+        fakeBookings: this.state.fakeBookings
+      });
+    } else {
+      this.setState({
+        fakeBookings: filteredSearch
+      });
+    }
   };
 
   render() {
@@ -13,7 +43,11 @@ export default class Bookings extends Component {
       <div className="App-content">
         <div className="container">
           <Search search={this.search} />
-          <SearchResults results={FakeBookings} />
+          {this.state.fakeBookings ? (
+            <SearchResults results={this.state.fakeBookings} />
+          ) : (
+            <h1>Waiting for data to be fetch...</h1>
+          )}
         </div>
       </div>
     );
--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,39 +1,56 @@
 import React from "react";
 import moment from "moment";
 
-const SearchResults = props => {
-  const tableData = props.results;
-  const tableInfoData = tableData.map(data => (
-    <tr>
-      <th scope="row">{data.id}</th>
-      <td>{data.title}</td>
-      <td>{data.firstName}</td>
-      <td>{data.surname}</td>
-      <td>{data.email}</td>
-      <td>{data.roomId}</td>
-      <td>{data.checkInDate}</td>
-      <td>{data.checkOutDate}</td>
-      <td>{moment(data.checkOutDate).diff(data.checkInDate, "days")}</td>
-    </tr>
-  ));
-  return (
-    <table class="table">
-      <thead>
-        <tr>
-          <th scope="col">id</th>
-          <th scope="col">title</th>
-          <th scope="col">first name</th>
-          <th scope="col">surname</th>
-          <th scope="col">email</th>
-          <th scope="col">room id</th>
-          <th scope="col">check in date</th>
-          <th scope="col">check out date</th>
-          <th scope="col">number of nights</th>
-        </tr>
-      </thead>
-      <tbody>{tableInfoData}</tbody>
-    </table>
-  );
-};
+class SearchResults extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isClicked: false
+    };
+  }
+  handleColorChange = () => {
+    this.setState({
+      isClicked: !this.state.isClicked
+    });
+  };
+  render() {
+    const tableData = this.props.results;
+    const tableInfoData = tableData.map(data => (
+      <tr
+        scope="row"
+        className={this.state.isClicked ? this.state.isClicked : "rowColor"}
+        onClick={this.handleColorChange}
+      >
+        <th>{data.id}</th>
+        <td>{data.title}</td>
+        <td>{data.firstName}</td>
+        <td>{data.surname}</td>
+        <td>{data.email}</td>
+        <td>{data.roomId}</td>
+        <td>{data.checkInDate}</td>
+        <td>{data.checkOutDate}</td>
+        <td>{moment(data.checkOutDate).diff(data.checkInDate, "days")}</td>
+      </tr>
+    ));
+    return (
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">id</th>
+            <th scope="col">title</th>
+            <th scope="col">first name</th>
+            <th scope="col">surname</th>
+            <th scope="col">email</th>
+            <th scope="col">room id</th>
+            <th scope="col">check in date</th>
+            <th scope="col">check out date</th>
+            <th scope="col">number of nights</th>
+          </tr>
+        </thead>
+        <tbody>{tableInfoData}</tbody>
+      </table>
+    );
+  }
+}
 
 export default SearchResults;
